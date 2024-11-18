e05ece0cd4a4a8fb29d86f9be606ae608d8695f9
Talha Ahmed <talha.ahmed@gmail.com>
2024-11-18 10:21:29 +0500

    Switching provider to openai

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,6 +1,6 @@
 local avante_opts = {
   ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-  provider = "ollama", -- Recommend using Claude
+  provider = "openai", -- Recommend using Claude
   auto_suggestions_provider = "ollama", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
   claude = {
     endpoint = "https://api.anthropic.com",
